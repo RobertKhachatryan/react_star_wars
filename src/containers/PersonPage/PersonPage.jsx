2a2530cdace375241a894dd3f00bd0e351de6715
Robert <robert.khachatryan2003@gmail.com>
2023-09-02 20:05:22 +0400

    Person info page fixes

--- src/containers/PersonPage/PersonPage.jsx
+++ src/containers/PersonPage/PersonPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getApiResource } from "@utils/network";
 import { API_PERSON } from "@constants/api";
@@ -6,15 +6,49 @@ import { API_PERSON } from "@constants/api";
 import styles from "./PersonPage.module.css";
 
 const PersonPage = () => {
+  //STATES
+  const [personInfo, setPersonInfo] = useState(null);
+  const [personName, setPersonName] = useState(null);
+  //STATES
   const { id } = useParams();
   useEffect(() => {
     (async () => {
       const res = await getApiResource(`${API_PERSON}/${id}/`);
-      console.log(res);
+      if (res) {
+        setPersonInfo([
+          { title: "Height", data: res.height },
+          { title: "Mass", data: res.mass },
+          { title: "Hair color", data: res.hair_color },
+          { title: "Skin color", data: res.skin_color },
+          { title: "Eye color", data: res.eye_color },
+          { title: "Birth year", data: res.birth_year },
+          { title: "Gender", data: res.gender },
+        ]);
+        setPersonName(res.name);
+      }
     })();
   }, []);
-
-  return <h1>Person Page</h1>;
+  console.log(personInfo);
+  return (
+    <>
+      <h1>{personName}</h1>
+      {personInfo && (
+        <ul>
+          {personInfo.map(({ title, data }) => {
+            return (
+              data && (
+                <li key={title}>
+                  <span>
+                    {title}: {data}
+                  </span>
+                </li>
+              )
+            );
+          })}
+        </ul>
+      )}
+    </>
+  );
 };
 
 export default PersonPage;
